82a8005fd0d4f5c683539393579ccb5b38cb14ab
macman1717 <cgriffin10@ggc.edu>
2025-02-07 20:52:32 -0500

    hi

--- backend/api/settings.py
+++ backend/api/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-e1zrho2bs$3=oq4jtn_rsno-$8@r^x5i3q0#5+#1e%u-iyc)*2
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
