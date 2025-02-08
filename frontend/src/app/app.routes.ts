2d8741c2a086bc5a2436b0bfb6cb8e364de833b5
macman1717 <cgriffin10@ggc.edu>
2025-02-08 13:58:33 -0500

    help

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,3 +1,6 @@
 import { Routes } from '@angular/router';
+import {ScratchPaperComponent} from "./scratch-paper/scratch-paper.component";
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {path:'scratch-paper', component:ScratchPaperComponent}
+];
